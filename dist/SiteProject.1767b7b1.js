1cf24d452b533adbb7abbf4aeed3ff95d809fea5
Shane Cummings <shanecummings_uk@yahoo.co.uk>
2025-04-24 18:54:36 +0100

    taken out the dist from gitignore temporally so netlify can try and build

---
+++ dist/SiteProject.1767b7b1.js
@@ -0,0 +1,2 @@
+let collapsibles=document.querySelectorAll(".collapsible");collapsibles.forEach(l=>l.addEventListener("click",function(){this.classList.toggle("collapsible--expanded")}));
+//# sourceMappingURL=SiteProject.1767b7b1.js.map
